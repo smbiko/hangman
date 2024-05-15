9bdd82fc45c1849fb05e95866e685c2004aeb34f
Siyasanga Mbiko <smbiko@gmail.com>
2024-05-15 20:36:53 +0000

    Build the word to show player

--- run.py
+++ run.py
@@ -36,3 +36,14 @@ def join_guessed_letters(guessed_letters):
     """
     return " ".join(sorted(guessed_letters))
 
+def build_guessed_word(target_word, guessed_letters):
+    """
+    Build the word to show player
+    """
+    current_letters= []
+    for letter in target_word:
+        if letter in guessed_letters:
+            current_letters.append(letter)
+        else:
+            current_letters.append("_")
+    return " ".join(current_letters)
